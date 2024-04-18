aef86db0627ab5bb4c6f89f7ee88944657b92760
Valber <valber_558@hotmail.com>
2024-04-18 14:22:42 -0300

    Requisito-06

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -3,16 +3,20 @@ package com.betrybe.museumfinder.controller;
 
 import com.betrybe.museumfinder.dto.MuseumCreationDto;
 import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
 import com.betrybe.museumfinder.util.ModelDtoConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Museum Controller.
@@ -34,7 +38,7 @@ public class MuseumController {
   }
 
   /**
-   *New Museum.
+   * New Museum.
    */
 
   @PostMapping
@@ -47,5 +51,21 @@ public class MuseumController {
     } catch (Exception e) {
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
+  }
+
+  /**
+   * Find de closest museum.
+   */
+  @GetMapping("/closest")
+  public ResponseEntity<MuseumDto> findClosestMuseum(
+      @RequestParam("lat") Double latitude,
+      @RequestParam("lng") Double longitude,
+      @RequestParam("max_dist_km") Double maxDistance) {
+
+    Coordinate coordinate = new Coordinate(latitude, longitude);
+    Museum closestMuseum = museumService.getClosestMuseum(coordinate, maxDistance);
+    MuseumDto museumDto = ModelDtoConverter.modelToDto(closestMuseum);
+
+    return ResponseEntity.ok(museumDto);
   }
 }
